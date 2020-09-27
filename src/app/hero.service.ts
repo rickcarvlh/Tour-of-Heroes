75a7154df183ac72ff3d8b45109d997c6ad302d5
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-27 22:25:55 +0100

    add services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 import { HEROES } from './mock-hero';
 
 @Injectable({
@@ -8,10 +9,12 @@ import { HEROES } from './mock-hero';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   // mostar a lista a inteira de herois
+  // enviar messagem após ir buscar is herois
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes')
     return of(HEROES);
   }
 }
